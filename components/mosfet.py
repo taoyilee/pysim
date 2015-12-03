4259a08d9288d4c7d77a84a6706b151031d3f310
Tao-Yi Lee <tylee@ieee.org>
2015-12-03 20:40:10 +0800

    Break up components

---
+++ components/mosfet.py
@@ -0,0 +1,11 @@
+class mosfet:
+    def __init__(self, inst_name, node1, node2, node3, value):
+        self.inst_name = inst_name
+        self.node1 = node1
+        self.node2 = node2
+        self.node3 = node3
+        self.value = value
+ 
+    def __str__(self):
+        return 'MOSFET ({0}, {1}, {2}, {3}, {4})'.format(
+            self.inst_name, self.node1, self.node2, self.node3, self.value)
--- parser.py
+++ parser.py
@@ -1,6 +1,13 @@
 import re
 import logging
-import components.source as source
+from components.voltage_source import voltage_source
+from components.current_source import current_source
+from components.resistor import resistor
+from components.capacitor import capacitor
+from components.inductor import inductor
+from components.diode import diode
+from components.mosfet import mosfet
+from components.bjt import bjt
 
 logger = logging.getLogger(__name__)
 ch = logging.StreamHandler()
@@ -29,20 +36,57 @@ def match_g2(line, prefix, class_header):
 	else:
 		return None
 
+def match_g3(line, prefix, class_header):
+	inst = re.match('(' + prefix + '\d+)\s+(\d+)\s+(\d+)\s+',line)
+	instval = re.match('(' + prefix + '\d+)\s+(\d+)\s+(\d+)\s+([\de\.]*)',line)
+	if inst:
+		if instval:
+			logger.debug('match('+ prefix +')->' + instval.group(1))
+			return	class_header(instval.group(1),instval.group(2),instval.group(3),instval.group(4))
+		else:
+			logger.debug('match('+ prefix +')->' + inst.group(1))
+			return	class_header(inst.group(1),inst.group(2),inst.group(3),None)
+	else:
+		return None
+
+def match_g4(line, prefix, class_header):
+	inst = re.match('(' + prefix + ')(\w)(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+',line)
+	instval = re.match('(' + prefix + ')(\w)(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+([\de\.]*)',line)
+	if inst:
+		if instval:
+			logger.debug('match('+ prefix +')->' + instval.group(1) + instval.group(2)+ instval.group(3))
+			return	class_header(instval.group(1)+ instval.group(2)+ instval.group(3), 
+				instval.group(2),instval.group(4),instval.group(5),instval.group(6))
+		else:
+			logger.debug('match('+ prefix +')->' + inst.group(1))
+			return	class_header(inst.group(1)+ instval.group(2)+ instval.group(3),
+				inst.group(4),inst.group(5),instval.group(4),None)
+	else:
+		return None
+
 def parse_voltageSource(line):
-	return match_g1(line, 'V', source.voltage_source)
+	return match_g1(line, 'V', voltage_source)
 
 def parse_currentSource(line):
-	return match_g2(line, 'I', source.current_source)
+	return match_g2(line, 'I', current_source)
 
 def parse_resistor(line):
-	return match_g2(line, 'R', source.resistor)
+	return match_g2(line, 'R', resistor)
 
 def parse_capacitor(line):
-	return match_g2(line, 'C', source.capacitor)
+	return match_g2(line, 'C', capacitor)
 
 def parse_inductor(line):
-	return match_g1(line, 'L', source.inductor)
+	return match_g1(line, 'L', inductor)
+
+def parse_diode(line):
+	return match_g3(line, 'D', diode)
+
+def parse_mosfet(line):
+	return match_g4(line, 'M', mosfet)
+
+def parse_bjt(line):
+	return match_g4(line, 'Q', bjt)
 
 def parse_netlist(file_name):
 	f = open(file_name, 'r')
@@ -69,17 +113,17 @@ def parse_netlist(file_name):
 		linst = parse_inductor(line)
 		if linst:
 			dev_list.append(linst)	
-		dinst = re.match('(D\d+)\s+(\d+)\s+(\d+)',line)
-		dinstval = re.match('(D\d+)\s+(\d+)\s+(\d+)\s+([\de\.]*)',line)
-		
-		qninstval = re.match('(QN\d+)\s+(d+)\s+(\d+)\s+(\d+)\s+([\de\.]*)',line)
-		qpinstval = re.match('(QP\d+)\s+(d+)\s+(\d+)\s+(\d+)\s+([\de\.]*)',line)
-		mninstval = re.match('(MN\d+)\s+(d+)\s+(\d+)\s+(\d+)\s+([\de\.]*)',line)
-		mpinstval = re.match('(MP\d+)\s+(d+)\s+(\d+)\s+(\d+)\s+([\de\.]*)',line)
 
-		qninst = re.match('(QN\d+)\s+(d+)\s+(\d+)\s+(\d+)\s+',line)
-		qpinst = re.match('(QP\d+)\s+(d+)\s+(\d+)\s+(\d+)\s+',line)
-		mninst = re.match('(MN\d+)\s+(d+)\s+(\d+)\s+(\d+)\s+',line)
-		mpinst = re.match('(MP\d+)\s+(d+)\s+(\d+)\s+(\d+)\s+',line)
+		dinst = parse_diode(line)
+		if dinst:
+			dev_list.append(dinst)	
+
+		qinst = parse_bjt(line)
+		if qinst:
+			dev_list.append(qinst)	
+
+		minst = parse_mosfet(line)
+		if qinst:
+			dev_list.append(minst)	
 
 	logger.debug(dev_list)
